8163d9663fb0790f1c3103e4cba42c6dab44c986
Dwi Setia Fardhana <dwi.setia.fardhana@gmail.com>
2023-05-31 04:53:46 +0700

    Seeder Tamu

---
+++ app/web/database/seeders/TamuSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tamu;
+use Carbon\Carbon;
+use Faker\Factory as Faker;
+use Illuminate\Database\Seeder;
+
+class TamuSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+        // Mendapatkan tanggal dan waktu saat ini
+        $currentDateTime = Carbon::now();
+
+       for($i = 0; $i < 50; $i++) {
+           // Mengatur rentang waktu satu minggu terakhir
+           $oneWeekAgo = $currentDateTime->subWeek();
+
+           // Menghasilkan tanggal dan jam dalam rentang satu minggu terakhir
+           $createdDateTime = $faker->dateTimeBetween($oneWeekAgo, $currentDateTime);
+
+           // Format tanggal dan jam sesuai kebutuhan Anda
+           $formattedDateTime = $createdDateTime->format('Y-m-d H:i:s');
+           $tujuan = $faker->numberBetween(1, 9);
+           Tamu::create([
+               'nama' => $faker->name,
+               'rumah_id' => $tujuan,
+               'plat' => generatePlatNomor($faker),
+               'approved_by' => $tujuan,
+               'created_at' => $createdDateTime,
+           ]);
+       }
+    }
+}
