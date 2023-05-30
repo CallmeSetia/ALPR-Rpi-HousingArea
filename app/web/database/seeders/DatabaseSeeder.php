fe076fb9d7fd7607ec5fe12611b22fa4fe3ef5f0
Dwi Setia Fardhana <dwi.setia.fardhana@gmail.com>
2023-05-31 04:51:14 +0700

    Init Seedder

--- app/web/database/seeders/DatabaseSeeder.php
+++ app/web/database/seeders/DatabaseSeeder.php
@@ -1,22 +1,20 @@
 <?php
-
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Rumah;
+use App\Models\PlatNomor;
+use App\Models\Tamu;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
+//        $this->call(\Database\Seeders\RumahSeeder::class);
+//        $this->call(\Database\Seeders\UserSeeder::class);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+//        $this->call(PlatNomorSeeder::class);
+        $this->call(TamuSeeder::class);
     }
 }
