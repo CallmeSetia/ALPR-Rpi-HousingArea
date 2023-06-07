9064025e6d265562b42a37c680dbfcd5514019e0
Dwi Setia Fardhana <dwi.setia.fardhana@gmail.com>
2023-06-07 13:25:29 +0700

    Update API USER PENJAGA DAN PENGHUNI

--- app/web/app/Http/Controllers/UserController.php
+++ app/web/app/Http/Controllers/UserController.php
@@ -2,9 +2,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\PlatNomor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
+// TODO: THROTTLE
 class UserController extends Controller
 {
     public function getUserPenjaga()
@@ -31,7 +33,27 @@ class UserController extends Controller
             // Contoh:
             $penghuni = User::where('role', 'Penghuni')->with('rumah')->get();
 
-            return response()->json($penghuni, 200);
+            $response = $penghuni->map(function ($penghuni) {
+                return [
+                    'id' => $penghuni->id,
+                    'username' => $penghuni->username,
+                    'role' => $penghuni->role,
+                    'created_at' => $penghuni->created_at,
+                    'updated_at' => $penghuni->updated_at,
+                    'nama' => $penghuni->nama,
+                    'rumah' => [
+                        'id' => $penghuni->rumah->id,
+                        'nama_pemilik' => $penghuni->rumah->nama_pemilik,
+                        'alamat' => $penghuni->rumah->alamat,
+                        'created_at' => $penghuni->rumah->created_at,
+                        'updated_at' => $penghuni->rumah->updated_at,
+                        'plat_nomor' => PlatNomor::where('rumah_id',  $penghuni->rumah->id)->get(),
+                    ],
+                ];
+            });
+
+            return response()->json($response, 200);
+
         } else {
             return response()->json(['message' => 'Unauthorized'], 401);
         }
--- app/web/routes/api.php
+++ app/web/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,6 +24,12 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::post('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('api.login');
-Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('api.logout');
 
+Route::middleware('auth:sanctum')
+    ->post('/logout', [App\Http\Controllers\AuthController::class, 'logout'])
+    ->name('api.logout');
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user/penjaga', [UserController::class, 'getUserPenjaga'])->name('api.user.penjaga');
+    Route::get('user/penghuni', [UserController::class, 'getUserPenghuni'])->name('api.user.penghuni');
+});
