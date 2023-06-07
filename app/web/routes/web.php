b0f355b4aacb200bcc72cb74085e22dbe0613909
Dwi Setia Fardhana <dwi.setia.fardhana@gmail.com>
2023-06-07 12:41:01 +0700

    WEB Routing

--- app/web/routes/web.php
+++ app/web/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +15,44 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Route::get('api/login', [App\Http\Controllers\AuthController::class, 'login']);
 
-Route::get('/{any}', function () {
-    return view('app');
-})->where("any",".*");
+// Last route
+//
+//Route::prefix('api')->group(function () {
+//    Route::get('login', [App\Http\Controllers\AuthController::class, 'login']);
+//});
+
+
+Route::get('/login/loading', function () {
+    return view('view.auth.loading');
+})->name('login.loading');;
+
+Route::get('/login', function () {
+    return view('view.auth.login');
+})->name('login');
+
+Route::get('/penghuni', function () {
+    return view('view.penghuni.penghuni');
+})->name('penghuni');
+
+
+Route::prefix('penjaga')->group(function () {
+
+    Route::get('/', function () {
+        return view('view.penjaga.penjaga');
+    })->name('penjaga.home');
+    Route::get('/control', function () {
+        return view('view.penjaga.control.control');
+    })->name('penjaga.control');
+
+    Route::get('/penghuni', function () {
+        return view('view.penjaga.penghuni.penghuni');
+    })->name('penjaga.penghuni');
+
+    Route::get('/tamu', function () {
+        return view('view.penjaga.tamu.tamu');
+    })->name('penjaga.tamu');
+});
+
+
