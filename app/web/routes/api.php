00944fae6727e2464399527540abebf1b666f82d
Dwi Setia Fardhana <dwi.setia.fardhana@gmail.com>
2023-06-07 12:46:29 +0700

    Update API Route Name

--- app/web/routes/api.php
+++ app/web/routes/api.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 //
 //});
 
-Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
-Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login'])->name('api.login');
+Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout'])->name('api.logout');
 
 
