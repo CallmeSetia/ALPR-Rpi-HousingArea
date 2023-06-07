ebce1ce80c33c90c5fb607788f5c2e208aa410fd
Dwi Setia Fardhana <dwi.setia.fardhana@gmail.com>
2023-06-07 13:30:04 +0700

    Update API USER PENJAGA DAN PENGHUNI

--- app/web/app/Http/Controllers/UserController.php
+++ app/web/app/Http/Controllers/UserController.php
@@ -58,4 +58,38 @@ class UserController extends Controller
             return response()->json(['message' => 'Unauthorized'], 401);
         }
     }
+    public function getUserTamu()
+    {
+        $user = Auth::user();
+
+        if ($user->role === 'Penghuni' || $user->role === 'Penjaga') {
+            // Lakukan operasi yang sesuai untuk mendapatkan data penghuni
+            // Contoh:
+            $penghuni = User::where('role', 'Penghuni')->with('rumah')->get();
+
+            $response = $penghuni->map(function ($penghuni) {
+                return [
+                    'id' => $penghuni->id,
+                    'username' => $penghuni->username,
+                    'role' => $penghuni->role,
+                    'created_at' => $penghuni->created_at,
+                    'updated_at' => $penghuni->updated_at,
+                    'nama' => $penghuni->nama,
+                    'rumah' => [
+                        'id' => $penghuni->rumah->id,
+                        'nama_pemilik' => $penghuni->rumah->nama_pemilik,
+                        'alamat' => $penghuni->rumah->alamat,
+                        'created_at' => $penghuni->rumah->created_at,
+                        'updated_at' => $penghuni->rumah->updated_at,
+                        'plat_nomor' => PlatNomor::where('rumah_id',  $penghuni->rumah->id)->get(),
+                    ],
+                ];
+            });
+
+            return response()->json($response, 200);
+
+        } else {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+    }
 }
