ba04b72cb1364d1008b419b980d2465eed18f22e
Dwi Setia Fardhana <dwi.setia.fardhana@gmail.com>
2023-06-07 12:43:29 +0700

    Model User Traits

--- app/web/app/Models/User.php
+++ app/web/app/Models/User.php
@@ -8,9 +8,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+use Laratrust\Traits\HasRolesAndPermissions;
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRolesAndPermissions;
 
     /**
      * The attributes that are mass assignable.
