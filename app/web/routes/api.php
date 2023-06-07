2829f12011ed2b655bdf0a9722a20c332290b875
Dwi Setia Fardhana <dwi.setia.fardhana@gmail.com>
2023-06-07 12:40:49 +0700

    Login API

--- app/web/routes/api.php
+++ app/web/routes/api.php
@@ -13,7 +13,16 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+//use App\Http\Controllers\AuthController;
+//
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::prefix('api')->group(function () {
+//
+//    Route::post('login', [App\Http\Controllers\AuthController::class, 'login']);
+//
+//});
+
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
+Route::post('/logout', [App\Http\Controllers\AuthController::class, 'logout']);
+
+
